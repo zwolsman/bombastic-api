375942e92773c70e3db18a8d836c23a544c46506
Marvin Zwolsman <mzwolsman@bol.com>
2022-01-03 17:59:01 +0100

    Rename package bombasticapi -> bombastic

--- src/main/kotlin/com/zwolsman/bombastic/BombasticApiApplication.kt
+++ src/main/kotlin/com/zwolsman/bombastic/BombasticApiApplication.kt
@@ -1,4 +1,4 @@
-package com.zwolsman.bombasticapi
+package com.zwolsman.bombastic
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/zwolsman/bombastic/BombasticApiApplicationTests.kt
+++ src/test/kotlin/com/zwolsman/bombastic/BombasticApiApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.zwolsman.bombasticapi
+package com.zwolsman.bombastic
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
