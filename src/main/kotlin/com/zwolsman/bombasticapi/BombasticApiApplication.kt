c61257d2c56131f5817fd6be62d8669f63246306
Marvin Zwolsman <mzwolsman@bol.com>
2022-01-03 17:56:35 +0100

    Initial commit

---
+++ src/main/kotlin/com/zwolsman/bombasticapi/BombasticApiApplication.kt
@@ -0,0 +1,11 @@
+package com.zwolsman.bombasticapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BombasticApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BombasticApiApplication>(*args)
+}
