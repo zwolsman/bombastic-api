1c154c51806ffda1830c6923a2e9981bc5880054
Marvin Zwolsman <mzwolsman@bol.com>
2022-01-06 14:17:02 +0100

    Validate sign in with Apple authCode

--- src/main/kotlin/com/zwolsman/bombastic/controllers/profile/ProfileController.kt
+++ src/main/kotlin/com/zwolsman/bombastic/controllers/profile/ProfileController.kt
@@ -15,8 +15,7 @@ class ProfileController(private val appleIdService: AppleIdService) {
 
     @GetMapping
     suspend fun verify(@RequestHeader(HttpHeaders.AUTHORIZATION) authHeader: String, @RequestParam authCode: String) {
-        val (_, base64Token) = authHeader.split(" ")
-        val identityToken = String(Base64.getDecoder().decode(base64Token))
+        val (_, identityToken) = authHeader.split(" ")
         appleIdService.verify(identityToken, authCode)
     }
 }
--- src/main/kotlin/com/zwolsman/bombastic/repositories/appleId/AppleIdRepository.kt
+++ src/main/kotlin/com/zwolsman/bombastic/repositories/appleId/AppleIdRepository.kt
@@ -1,10 +1,16 @@
 package com.zwolsman.bombastic.repositories.appleId
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.zwolsman.bombastic.config.AppleIdConfiguration
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
+import org.springframework.http.MediaType
+import org.springframework.http.codec.ClientCodecConfigurer
+import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.stereotype.Repository
 import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import org.springframework.web.reactive.function.client.awaitBody
@@ -18,9 +24,25 @@ class AppleIdRepository(
     private val log = LoggerFactory.getLogger(javaClass)
 
     @SpringConfiguration
-    class Configuration {
+    class Configuration(objectMapper: ObjectMapper) {
+        private val snakeCaseObjectMapper = objectMapper.copy().apply {
+            propertyNamingStrategy =
+                PropertyNamingStrategies.SNAKE_CASE
+        }
+
+        private val strategies = ExchangeStrategies
+            .builder()
+            .codecs { clientDefaultCodecsConfigurer: ClientCodecConfigurer ->
+                clientDefaultCodecsConfigurer.defaultCodecs()
+                    .jackson2JsonDecoder(Jackson2JsonDecoder(snakeCaseObjectMapper, MediaType.APPLICATION_JSON))
+            }.build()
+
         @Bean
-        fun appleIdWebClient() = WebClient.create("https://appleid.apple.com")
+        fun appleIdWebClient() = WebClient
+            .builder()
+            .baseUrl("https://appleid.apple.com")
+            .exchangeStrategies(strategies)
+            .build()
     }
 
     suspend fun validateCode(secret: String, authCode: String): TokenResponse {
