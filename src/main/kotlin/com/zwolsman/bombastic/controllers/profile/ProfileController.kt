90a9c599ab73677d78249d4f896290a596689b18
Marvin Zwolsman <mzwolsman@bol.com>
2022-04-19 13:26:12 +0200

    feat: add transactions endpoint

--- src/main/kotlin/com/zwolsman/bombastic/controllers/profile/ProfileController.kt
+++ src/main/kotlin/com/zwolsman/bombastic/controllers/profile/ProfileController.kt
@@ -1,8 +1,13 @@
 package com.zwolsman.bombastic.controllers.profile
 
 import com.zwolsman.bombastic.controllers.profile.responses.ProfileResponse
+import com.zwolsman.bombastic.controllers.profile.responses.TransactionResponse
+import com.zwolsman.bombastic.controllers.profile.responses.TransactionType
+import com.zwolsman.bombastic.controllers.profile.responses.TransactionsResponse
 import com.zwolsman.bombastic.domain.Profile
 import com.zwolsman.bombastic.services.ProfileService
+import com.zwolsman.bombastic.services.TransactionService
+import org.bitcoinj.core.NetworkParameters
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,12 +16,43 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/profiles")
-class ProfileController(private val profileService: ProfileService) {
+class ProfileController(
+    private val profileService: ProfileService,
+    private val transactionService: TransactionService,
+    private val params: NetworkParameters,
+) {
 
     @GetMapping("/me")
     suspend fun userProfile(@AuthenticationPrincipal profile: Profile): ProfileResponse =
         profile
             .let(::ProfileResponse)
+
+    @GetMapping("/me/transactions")
+    suspend fun userProfile(@AuthenticationPrincipal profile: Profile, unused: Int? = null): TransactionsResponse {
+        val transactions = transactionService.findTransactions(profile)
+
+        // TODO: create response mapper
+        val transactionResponses = transactions.flatMap { tx ->
+            tx.outputs.filter { output ->
+                output.scriptPubKey.getToAddress(params).toString() == profile.address
+            }
+                .map { output ->
+                    val amount = output.value.toSat()
+                    TransactionResponse(
+                        txId = tx.txId.toString(),
+                        type = TransactionType.DEPOSIT,
+                        amount = amount,
+                        timestamp = tx.updateTime,
+                        confirmed = tx.confidence.depthInBlocks >= 1
+                    )
+                }
+        }
+        return TransactionsResponse(
+            name = profile.name,
+            address = profile.address,
+            transactions = transactionResponses
+        )
+    }
 
     @GetMapping("/{id}")
     suspend fun byId(@PathVariable id: String): ProfileResponse =
