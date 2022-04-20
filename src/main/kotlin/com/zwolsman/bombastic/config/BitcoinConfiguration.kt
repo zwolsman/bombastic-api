cecdd6da55e70930cbb1fe4972e231c069532b04
Marvin Zwolsman <mzwolsman@bol.com>
2022-04-20 11:25:00 +0200

    feat: save/load wallet from local disk

--- src/main/kotlin/com/zwolsman/bombastic/config/BitcoinConfiguration.kt
+++ src/main/kotlin/com/zwolsman/bombastic/config/BitcoinConfiguration.kt
@@ -4,16 +4,17 @@ import org.bitcoinj.core.AbstractBlockChain
 import org.bitcoinj.core.BlockChain
 import org.bitcoinj.core.NetworkParameters
 import org.bitcoinj.core.PeerGroup
-import org.bitcoinj.core.UTXOProvider
 import org.bitcoinj.crypto.DeterministicKey
 import org.bitcoinj.script.Script
 import org.bitcoinj.store.BlockStore
-import org.bitcoinj.store.PostgresFullPrunedBlockStore
+import org.bitcoinj.store.SPVBlockStore
 import org.bitcoinj.wallet.Wallet
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 @Configuration
 class BitcoinConfiguration(
@@ -28,13 +29,24 @@ class BitcoinConfiguration(
         NetworkParameters.fromPmtProtocolID(paymentProtocolId) ?: error("Unrecognized payment protocol id")
 
     @Bean
-    fun blockStore(params: NetworkParameters) = SPVBlockStore(params, File("block-store-${params.paymentProtocolId}.dat"))
+    fun blockStore(params: NetworkParameters) =
+        SPVBlockStore(params, File("block-store-${params.paymentProtocolId}.dat"))
 
     @Bean
-    fun wallet(params: NetworkParameters, utxoProvider: UTXOProvider): Wallet {
-        val key = DeterministicKey.deserializeB58(watchKeyData, params)
-        val wallet = Wallet.fromWatchingKey(params, key, Script.ScriptType.P2PKH)
-        wallet.utxoProvider = utxoProvider
+    fun wallet(params: NetworkParameters): Wallet {
+        val file = File("wallet-${params.paymentProtocolId}.dat")
+        log.info("Looking for wallet file ${file.absolutePath}")
+        val wallet = if (file.exists()) {
+            Wallet.loadFromFile(file)
+                .also { log.info("Restoring wallet from file") }
+        } else {
+            val key = DeterministicKey.deserializeB58(watchKeyData, params)
+            log.warn("No wallet found. Using the watching key to create one")
+            Wallet.fromWatchingKey(params, key, Script.ScriptType.P2WPKH)
+        }
+
+        wallet.autosaveToFile(file, 1000, TimeUnit.MILLISECONDS, null)
+
         return wallet
     }
 
@@ -44,8 +56,7 @@ class BitcoinConfiguration(
         peerGroup.addWallet(wallet)
         peerGroup.start()
         peerGroup.downloadBlockChain()
-        log.info("Addresses that we know: ${wallet.issuedReceiveAddresses.map { it.hash }}")
-        log.info("Address: ${wallet.freshReceiveAddress()}")
+        log.info("Issued receive addresses: ${wallet.issuedReceiveAddresses.map { it.toString() }}")
         return peerGroup
     }
 
